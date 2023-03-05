38aa1ccaa7795f29cf1189639374a1b95a559498
Pablo García Ruiz <pgruiz727@gmail.com>
2023-03-05 11:59:24 +0100

    Implement http module and tcp client + server

---
+++ http/src/httprequest.rs
@@ -0,0 +1,126 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct HttpRequest {
+    pub method: Method,
+    pub version: Version,
+    pub resource: Resource,
+    pub headers: HashMap<String, String>,
+    pub msg_body: String,
+}
+
+impl From<String> for HttpRequest {
+    fn from(req: String) -> Self {
+        let mut parsed_method = Method::UNINITIALIZED;
+        let mut parsed_version = Version::V1_1;
+        let mut parsed_resource = Resource::Path("".to_string());
+        let mut parsed_headers = HashMap::new();
+        let mut parsed_msg_body = "";
+
+        for line in req.lines() {
+            if line.contains("HTTP") {
+                let (method, resource, version) = process_req_line(line);
+                parsed_method = method;
+                parsed_version = version;
+                parsed_resource = resource;
+            } else if line.contains(":") {
+                let (key, value) = process_header_line(line);
+                parsed_headers.insert(key, value);
+            } else if line.len() == 0 {
+            } else {
+                parsed_msg_body = line;
+            }
+        }
+        // Parse the incoming HTTP request into HttpRequest struct
+        HttpRequest {
+            method: parsed_method,
+            version: parsed_version,
+            resource: parsed_resource,
+            headers: parsed_headers,
+            msg_body: parsed_msg_body.to_string(),
+        }
+    }
+}
+
+fn process_req_line(s: &str) -> (Method, Resource, Version) {
+    let mut words = s.split_whitespace();
+    let method = words.next().unwrap();
+    let resource = words.next().unwrap();
+    let version = words.next().unwrap();
+
+    (
+        method.into(),
+        Resource::Path(resource.to_string()),
+        version.into(),
+    )
+}
+
+fn process_header_line(s: &str) -> (String, String) {
+    let mut header_items = s.split(":");
+    let mut key = String::from("");
+    let mut value = String::from("");
+    if let Some(k) = header_items.next() {
+        key = k.to_string();
+    }
+    if let Some(v) = header_items.next() {
+        value = v.to_string()
+    }
+
+    (key, value)
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Resource {
+    Path(String),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Method {
+    GET,
+    POST,
+    UNINITIALIZED,
+}
+
+impl From<&str> for Method {
+    fn from(s: &str) -> Self {
+        match s {
+            "GET" => Method::GET,
+            "POST" => Method::POST,
+            _ => Method::UNINITIALIZED,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Version {
+    V1_1,
+    V2_0,
+    UNINITIALIZED,
+}
+
+impl From<&str> for Version {
+    fn from(s: &str) -> Self {
+        match s {
+            "HTTP/1.1" => Version::V1_1,
+            "HTTP/2.0" => Version::V2_0,
+            _ => Version::UNINITIALIZED,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_method_into() {
+        let m: Method = "GET".into();
+        assert_eq!(m, Method::GET);
+    }
+
+    #[test]
+    fn test_version_into() {
+        let m: Version = "HTTP/1.1".into();
+        assert_eq!(m, Version::V1_1);
+    }
+}
